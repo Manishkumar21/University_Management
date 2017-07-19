d3c6e19ba7820f03790c6913ccd8cee4a1af042f
Manishkumar21 <khiladimanu1@gmail.com>
2017-07-19 22:29:27 +0530

    Enroll Student File and Index File Added

---
+++ index.php
@@ -0,0 +1,117 @@
+
+<html>
+<head>
+
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+	
+	<link href="css/style.css" rel="stylesheet" type="text/css">
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+	
+	    <!-- Custom CSS -->
+    <link href="css/sb-admin.css" rel="stylesheet">
+	
+	    <!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+	
+	<title>University Management</title>
+</head>
+<body>
+<div class="outer">
+<div class="inner">
+<div class="top">
+<h1>HomePage <font size="6px" color="grey">Login</font></h1>
+</div><hr>
+<ol class="breadcrumb">
+    <li class="active">
+        <i class="fa fa-dashboard"></i> Home
+    </li>
+</ol>
+<!-- /.row -->
+<div class="row">
+	<div class="col-lg-12">
+		<div class="alert alert-info alert-dismissable">
+			<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+			<i class="fa fa-info-circle"></i>  <strong>Please Login</strong> first for Details!
+		</div>
+	</div>
+</div>
+                <!-- /.row -->
+
+
+<div class="row">
+	<div class="col-lg-4 col-md-6">
+	<form name="form1" method="post" action="action2.php">
+		<div class="panel panel-primary">
+			<div class="panel-heading">
+				<div class="row">
+					<div class="col-xs-3">
+						<i class="fa fa-lock fa-5x"></i>
+					</div>
+					<div class="col-xs-9 text-right">
+						<div class="huge">Admin Login.</div>
+						<div>Admin Login.</div>
+					</div>
+				</div>
+			</div>
+				<div class="panel-footer">
+					<span class="pull-left">	<i class="fa fa-arrow-circle-right"></i>	</span>
+					<span class="pull-right">	<input type="submit" name="submit1" value="LogIn"></span>
+					<div class="clearfix"></div>
+				</div>
+		</div></form>
+	</div>
+	<div class="col-lg-4 col-md-6">
+	<form name="nnn" method="post" action="action4.php">
+		<div class="panel panel-green">
+			<div class="panel-heading">
+				<div class="row">
+					<div class="col-xs-3">
+						<i class="fa fa-user fa-5x"></i>
+					</div>
+					<div class="col-xs-9 text-right">
+						<div class="huge">Teacher Login.</div>
+						<div>Teacher Login.</div>
+					</div>
+				</div>
+			</div>
+				<div class="panel-footer">
+					<span class="pull-left">	<i class="fa fa-arrow-circle-right"></i>	</span>
+					<span class="pull-right">	<input type="submit" name="submit2" value="LogIn">	</span>
+					<div class="clearfix"></div>
+				</div>
+		</div></form>
+	</div><form name="form1" method="post" action="action3.php">
+	<div class="col-lg-4 col-md-6">
+		<div class="panel panel-yellow">
+			<div class="panel-heading">
+				<div class="row">
+					<div class="col-xs-3">
+						<i class="fa fa-sign-in fa-5x"></i>
+					</div>
+					<div class="col-xs-9 text-right">
+						<div class="huge">Student Login.</div>
+						<div>Student Login.</div>
+					</div>
+				</div>
+			</div>
+				<div class="panel-footer">
+					<span class="pull-left">	<i class="fa fa-arrow-circle-right"></i>	</span>
+					<span class="pull-right"><input type="submit" name="submit3" value="Enter">	</span>
+					<div class="clearfix"></div>
+				</div>
+		</div>
+	</div></form>
+</div>
+	
+<!-- /.row -->
+
+</div>
+</div>
+</body>
+</html>
